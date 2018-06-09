e5e0f1687aa1bdf40e64a8611dec9f8d37e5623e
krisxlle <kriselle.t@gmail.com>
2018-06-09 14:12:28 -0700

    project vars

--- projects.js
+++ projects.js
@@ -2,8 +2,9 @@
 var kimika {
     date: "May 26-27, 2018",
     type: "computer science",
-    imgsrc: "",
-    link: "",
+    imgsrc: "urea.png",
+    caption: "Example molecule that can be created on Kimika.",
+    link: "https://github.com/jeanellet/kimika",
     name: "Kimika",
     description: "An interactive 'crafting' game that can be used to learn about basic chemistry.",
     credits: "Jeanelle Tanhueco"
@@ -13,6 +14,7 @@ var cloud {
     date: "April 2018",
     type: "engineering",
     imgsrc: "",
+    caption: "Inner workings of cloud (lanterns, wires and LEDs connected to Arduino).",
     name: "Sound Reactive Lightning Cloud",
     description: "A cloud-shaped lamp with LEDs that light up based on sound received by a microphone.",
     credits: "Ashley Chu, Karla Jauregui Sandoval, and Samantha McDonough"
@@ -22,12 +24,15 @@ var hoodie {
     date: "February 13, 2017",
     type: "art",
     imgsrc: "hoodie.jpg",
+    caption: "Hoodie design before being printed onto hoodies.",
     name: "Class Hoodie Design",
     description: "Design created on Adobe Photoshop for use in class merchandise."
 };
 
 var asl {
-    date: "July 2017"
-}
+    date: "July 2017",
+    type: "computer science",
+    imgsrc: "",
+};
 
 var projects = [kimika, cloud, hoodie, asl];
