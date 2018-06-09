89dbc9adcb261c66c9846dba31881a11d85cae47
krisxlle <kriselle.t@gmail.com>
2018-06-09 14:00:50 -0700

    started project vars

---
+++ projects.js
@@ -0,0 +1,33 @@
+
+var kimika {
+    date: "May 26-27, 2018",
+    type: "computer science",
+    imgsrc: "",
+    link: "",
+    name: "Kimika",
+    description: "An interactive 'crafting' game that can be used to learn about basic chemistry.",
+    credits: "Jeanelle Tanhueco"
+};
+
+var cloud {
+    date: "April 2018",
+    type: "engineering",
+    imgsrc: "",
+    name: "Sound Reactive Lightning Cloud",
+    description: "A cloud-shaped lamp with LEDs that light up based on sound received by a microphone.",
+    credits: "Ashley Chu, Karla Jauregui Sandoval, and Samantha McDonough"
+};
+
+var hoodie {
+    date: "February 13, 2017",
+    type: "art",
+    imgsrc: "hoodie.jpg",
+    name: "Class Hoodie Design",
+    description: "Design created on Adobe Photoshop for use in class merchandise."
+};
+
+var asl {
+    date: "July 2017"
+}
+
+var projects = [kimika, cloud, hoodie, asl];
