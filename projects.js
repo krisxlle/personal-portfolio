aacb8eb28e716c5a04434be84b9389d15dc846fe
krisxlle <kriselle.t@gmail.com>
2018-06-09 15:15:09 -0700

    added all vars

--- projects.js
+++ projects.js
@@ -13,7 +13,7 @@ var kimika {
 var cloud {
     date: "April 2018",
     type: "engineering",
-    imgsrc: "",
+    imgsrc: "cloud.mp4",
     caption: "Inner workings of cloud (lanterns, wires and LEDs connected to Arduino).",
     name: "Sound Reactive Lightning Cloud",
     description: "A cloud-shaped lamp with LEDs that light up based on sound received by a microphone.",
@@ -33,6 +33,45 @@ var asl {
     date: "July 2017",
     type: "computer science",
     imgsrc: "",
+    caption: "",
+    link: "",
+    name: "Sign Academy",
+    description: "Website designed to teach users basic American Sign Language (ASL).",
+    credits: "Ashley Sanchez, Jianna Li, and Rene Stroughter"
 };
 
-var projects = [kimika, cloud, hoodie, asl];
+var gunControl {
+    date: "April 20, 2018",
+    type: "leadership",
+    imgsrc: "", /*there's a video of this somewhere on my phone*/
+    caption: "",
+    name: "Gun Control Walkout",
+    description: "Led and organized walkout calling for increased gun control.",
+    credits: "Dorothy Le and Mindy Diep"
+};
+
+var googleInterview {
+    date: "August 7, 2017",
+    type: "leadership",
+    imgsrc: "",
+    caption: "",
+    link: "https://www.youtube.com/watch?v=ClVi_OfnSI8",
+    name: "Google Interview",
+    description: "Interview by KPIX about Google Manifesto, which states that women do not belong in the tech industry.",
+    credits: "KPIX-TV"
+};
+
+var walkoutInterview {
+    date: "November 10, 2016",
+    type: "leadership",
+    imgsrc: "",
+    caption: "",
+    link: "", /*actual interview is in youtube video. find file on computer or extract from video."*/
+    name: "Kron4 Interview",
+    description: "Interview by Kron4 News about participation in protest against Donald Trump.",
+    credits: "KRON-TV"
+};
+
+
+
+var projects = [kimika, cloud, hoodie, asl, gunControl, googleInterview, walkoutInterview];
